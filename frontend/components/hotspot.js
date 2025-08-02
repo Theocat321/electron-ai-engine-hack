f584a04b02d06a7163ca35ff2fa56c10ccfac1c2
Adam O'Neill <adamo.mark25@gmail.com>
2025-08-02 17:46:04 +0100

    Update following iter 1

--- frontend/components/hotspot.js
+++ frontend/components/hotspot.js
@@ -1,8 +1,8 @@
 export function createHotspot(x, y, onClick) {
     const div = document.createElement('div');
     div.className = 'hotspot';
-    div.style.left = `${x}px`;
-    div.style.top = `${y}px`;
+    div.style.left = `${x - 10}px`;
+    div.style.top = `${y - 10}px`;
 
     div.addEventListener('click', (e) => {
         e.stopPropagation();
